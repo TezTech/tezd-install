58b15d802782f9aee99b767bd0ab9a32392e8985
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-11-22 19:00:44 +1300

    Removing tzproxy and fixing

--- tezd-install.sh
+++ tezd-install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
+HPATH=/usr/lib/tezd
 echo "Running TezTech Tezos Daemon (tezd)..."
-mkdir tezd
-cd tezd
-current_dir=$PWD
-echo $PWD
 
-echo "Building Tezos Core..."
 echo "Installing deps..."
 if ! grep -q "^deb .*ansible/bubblewrap" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
 	echo "Installing bubblewrap PPA..."
@@ -17,100 +13,112 @@ if ! grep -q "^deb .*git-core/ppa" /etc/apt/sources.list /etc/apt/sources.list.d
 fi
 sudo apt-get update
 sudo apt-get install -y wget liblz4-tool patch unzip make gcc m4 git g++ aspcud bubblewrap curl bzip2 rsync libev-dev libgmp-dev pkg-config libhidapi-dev
-curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
-sudo apt install nodejs
-sudo npm install -g forever
 
 echo "Installing opam..."
 wget https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-x86_64-linux
 sudo mv opam-2.0.0-x86_64-linux /usr/local/bin/opam
 sudo chmod a+x /usr/local/bin/opam
-yes | opam init --compiler=4.06.1
-eval $(opam env)
 
-echo "Installing tezos..."
-git clone -b mainnet https://gitlab.com/tezos/tezos.git
-cd tezos
+echo "Creating tezd user..."
+useradd -r tezd --shell /bin/bash --home $HPATH -m
+adduser tezd sudo
 
-echo "Getting build deps..."
-make build-deps
-eval $(opam env)
-echo "Making..."
-make
-cd $current_dir
-echo "Tezos Core built!"
+su tezd -c "opam init -y --compiler=4.06.1"
+su tezd -c "eval \$(opam env)"
+cd $HPATH
 
-echo "Building TZProxy..."
-git clone -b master https://github.com/TezTech/tzproxy.git
-cd tzproxy
-npm install
-cd $current_dir
-echo "TZProxy built!"
+echo "Building tezos-core..."
+su tezd -c "git clone -b mainnet https://gitlab.com/tezos/tezos.git"
+sleep 5
+cd $HPATH/tezos
+su tezd -c "make build-deps && eval \$(opam env) && make"
+cd $HPATH
 
 echo "Building scripts..."
-mkdir scripts
-cd scripts
-cat > run.sh << EOF
+mkdir $HPATH/scripts
+cd $HPATH/scripts
+cat > start.sh << EOF
 #!/bin/bash
-echo "Running tzProxy...\n"
-if [ ! -f "$HOME/.tezos-node/config.json" ]; then echo "Creating config..."; $HOME/tezos/tezos-node config init --rpc-addr 127.0.0.1:8732; fi
-if [ ! -f "$HOME/.tezos-node/identity.json" ]; then $HOME/tezos/tezos-node identity generate 26.; fi
+echo "Starting tezd...\n"
+if [ ! -f "$HPATH/.tezos-node/config.json" ]; then 
+	echo "Creating config..."
+	PEERS=\$(curl -s 'http://api5.tzscan.io/v1/network?state=running&p=0&number=50' | grep -Po '::ffff:([0-9.:]+)' | sed ':a;N;\$!ba;s/\n/ /g' | sed 's/::ffff:/--peer=/g')
+	su tezd -c "$HPATH/tezos/tezos-node config init --data-dir $HPATH/.tezos-node --rpc-addr 127.0.0.1:8732 \$PEERS"
+fi
+if [ ! -f "$HPATH/.tezos-node/identity.json" ]; then 
+  su tezd -c "$HPATH/tezos/tezos-node identity generate 26. --data-dir $HPATH/.tezos-node"; 
+    fi
 echo "Starting node..."
-nohup $current_dir/tezos/tezos-node run > $current_dir/node.log &
-echo "Starting proxy..."
-forever -o $current_dir/proxy.log start $current_dir/tzproxy/index.js
+su tezd -c "nohup $HPATH/tezos/tezos-node run > $HPATH/node.log &"
 EOF
 cat > stop.sh << EOF
 #!/bin/bash
-echo "Stopping tzProxy...\n"
-forever stopall
+echo "Stopping tezd...\n"
 pkill -9 tezos-node
 EOF
 cat > restart.sh << EOF
 #!/bin/bash
-sh stop.sh
-sh run.sh
+sh $HPATH/scripts/stop.sh
+sh $HPATH/scripts/start.sh
 EOF
 cat > update.sh << EOF
 #!/bin/bash
-sh stop.sh
-cd tezos && git checkout mainnet && git pull && eval $(opam env) && make && cd $current_dir
-sh run.sh
+sh $HPATH/scripts/stop.sh
+su tezd -c "cd $HPATH/tezos && git fetch && git rebase && eval \$(opam env) && make && cd $HPATH"
+sh $HPATH/scripts/start.sh
+EOF
+cat > rebuild.sh << EOF
+#!/bin/bash
+sh $HPATH/scripts/stop.sh
+cd $HPATH/tezos
+su tezd -c "sh <(curl -sL https://raw.githubusercontent.com/TezTech/tezd-install/master/tezd-rebuild.sh)"
+su tezd -c "\$(opam env) && make build-deps && make"
+sh $HPATH/scripts/start.sh
 EOF
 cat > setup.sh << EOF
 #!/bin/bash
-sh stop.sh
-if [ -f "$HOME/.tezos-node/config.json" ]; then rm -f "$HOME/.tezos-node/config.json"; fi
-if [ -f "$HOME/.tezos-node/identity.json" ]; then rm -f "$HOME/.tezos-node/identity.json"; fi
-sh run.sh
+sh $HPATH/scripts/stop.sh
+if [ -f "$HPATH/.tezos-node/config.json" ]; then rm -f "$HPATH/.tezos-node/config.json"; fi
+if [ -f "$HPATH/.tezos-node/identity.json" ]; then rm -f "$HPATH/.tezos-node/identity.json"; fi
+sh $HPATH/scripts/start.sh
 EOF
-cd $current_dir
-echo "scripts built!"
-echo "Downloading quicksync..."
-mkdir .tezos-node
-cd .tezos-node
-wget http://quicksync.tzdutch.com/latest.tar.lz4
-lz4 -d latest.tar.lz4 | tar xf -
-cat > version.json << EOF
-{ "version": "0.0.1" }
-EOF
-cd $current_dir
-echo "quicksync done!"
-
+cd $HPATH
 cat > tezd.sh << EOF
 #!/bin/bash
 if test "\$1" = 'stop'; then
-	sh scripts/stop.sh
+	sh $HPATH/scripts/stop.sh
 fi
-if test "\$1" = 'run';  then
-	sh scripts/run.sh
+if test "\$1" = 'start';  then
+	sh $HPATH/scripts/start.sh
 fi
 if test "\$1" = 'restart'; then
-	sh scripts/restart.sh
+	sh $HPATH/scripts/restart.sh
 fi
 if test "\$1" = 'update'; then
-	sh scripts/update.sh
+	sh $HPATH/scripts/update.sh
+fi
+if test "\$1" = 'rebuild'; then
+	sh $HPATH/scripts/rebuild.sh
+fi
+if test "\$1" = 'setup'; then
+	sh $HPATH/scripts/setup.sh
 fi
 EOF
-echo "tezd install successful!"
+if [ ! -d "$HPATH/.tezos-node" ]; then 
+	echo "Downloading quicksync..."
+	mkdir $HPATH/.tezos-node
+	cd $HPATH/.tezos-node
+	wget http://quicksync.tzdutch.com/latest.tar.lz4
+	lz4 -d latest.tar.lz4 | tar xf -
+	cat > version.json << EOF
+{ "version": "0.0.1" }
+EOF
+	cd $HPATH
+fi
 
+chown -rf tezd:tezd $HPATH/.tezos-node
+chown -rf tezd:tezd $HPATH/scripts
+chown -rf tezd:tezd $HPATH/tezd.sh
+
+sh $HPATH/tezd.sh setup
+echo "tezd install complete!"
