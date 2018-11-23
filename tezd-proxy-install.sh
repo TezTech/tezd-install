b7dc574e6bcd0ead89fb5733f2fd62ee447e01a1
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-11-23 15:13:17 +1300

    Update tezd-proxy-install.sh

--- tezd-proxy-install.sh
+++ tezd-proxy-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
 curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
-sudo apt install nodejs
+sudo apt install -y nodejs
 sudo npm install -g forever
 git clone -b master https://github.com/TezTech/tzproxy.git
 cd tzproxy
