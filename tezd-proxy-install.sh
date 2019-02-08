f743e37f835c66361f43cbaad9f74c2c560114bd
Stephen Andrews <stephen@andrewstribe.co.nz>
2019-02-09 11:24:27 +1300

    Update tezd-proxy-install.sh

--- tezd-proxy-install.sh
+++ tezd-proxy-install.sh
@@ -3,4 +3,5 @@ sudo apt-get -y install nginx
 sudo update-rc.d nginx enable
 sudo rm -f /etc/nginx/nginx.conf
 wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/TezTech/tzproxy/master/nginx.conf
+sudo nginx -s reload
 exit
