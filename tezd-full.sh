d92b752e943a21aaa00a53edeac7228c46f1050d
Stephen Andrews <stephen@andrewstribe.co.nz>
2019-02-09 13:41:58 +1300

    Update tezd-full.sh

--- tezd-full.sh
+++ tezd-full.sh
@@ -2,6 +2,6 @@
 sudo apt-get update && apt-get dist-upgrade -y
 
 sh -c "$(curl -sL https://raw.githubusercontent.com/TezTech/tezd-install/master/tezd-install.sh)"
-sh -c "$(curl -sL https://raw.githubusercontent.com/TezTech/tezd-install/master/tezd-proxy-install.sh)"
+sh -c "$(curl -sL https://raw.githubusercontent.com/TezTech/tzproxy/master/install.sh)"
 
 (crontab -l ; echo "@reboot tezd start") | crontab
