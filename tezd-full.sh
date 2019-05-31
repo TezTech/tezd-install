32216938701352f51132d5eab6cca6ec3d950a34
Stephen Andrews <stephen@andrewstribe.co.nz>
2019-05-31 13:28:32 +1200

    Update tezd-full.sh

--- tezd-full.sh
+++ tezd-full.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo dpkg --configure -a
-sudo apt-get update && apt-get dist-upgrade -y
+sudo apt-get update
 
 sh -c "$(curl -sL https://raw.githubusercontent.com/TezTech/tezd-install/master/tezd-install.sh)"
 sh -c "$(curl -sL https://raw.githubusercontent.com/TezTech/tzproxy/master/install.sh)"
